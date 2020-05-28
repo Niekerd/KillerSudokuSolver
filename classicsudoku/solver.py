a590f29d737f37c965c885ac59f0175d8295c5fd
Niekerd <n.dooper@student.avans.nl>
2020-05-28 03:39:39 +0200

    better find_next function for classic sudoku

--- classicsudoku/solver.py
+++ classicsudoku/solver.py
@@ -1,3 +1,5 @@
+from time import process_time
+
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -10,8 +12,22 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def solve(bo):
-    find = find_empty(bo)
+expertBoard = [
+    [0,5,0,0,0,0,0,0,0],
+    [4,6,9,0,0,0,0,0,5],
+    [0,0,0,0,0,9,3,0,0],
+    [0,0,0,5,0,7,2,0,0],
+    [1,0,0,0,3,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0],
+    [6,0,0,0,0,0,0,0,7],
+    [7,0,4,2,0,0,1,0,0],
+    [8,0,0,6,0,0,0,4,2]
+]
+
+steps = 0
+
+def solve(bo, pos):
+    find = find_next(bo, pos)
     if not find:
         return True
     else:
@@ -21,7 +37,7 @@ def solve(bo):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
 
-            if solve(bo):
+            if solve(bo, (row, col)):
                 return True
 
             bo[row][col] = 0
@@ -30,6 +46,8 @@ def solve(bo):
 
 
 def valid(bo, num, pos):
+    global steps
+
     # Check row
     for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
@@ -48,7 +66,7 @@ def valid(bo, num, pos):
         for j in range(box_x * 3, box_x*3 + 3):
             if bo[i][j] == num and (i,j) != pos:
                 return False
-
+    steps += 1
     return True
 
 
@@ -66,16 +84,38 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
+# simpler version of find_next, slower than find_next
+# def find_empty(bo):
+#     for i in range(len(bo)):
+#         for j in range(len(bo[0])):
+#             if bo[i][j] == 0:
+#                 return (i, j)  # row, col
+#     return None
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
+def find_next(bo, pos):
+    # find_next remembers the previous tile and finds the next in order, instead of starting from 0,0 again like find_empty.
+    p0 = pos[0]
+    p1 = pos[1] 
+    if bo[p0][p1] == 0:
+        return (pos)
+    else:
+        if pos[1] == 8 and pos[0] != 8:
+            nextRow = pos[0] + 1
+            return find_next(bo, (nextRow, 0))
 
-    return None
+        if pos[1] == 8 and pos[0] == 8:
+            return None
+        else: 
+            nextColumn = pos[1] + 1
+            return find_next(bo, (pos[0], nextColumn))
 
-print_board(board)
-solve(board)
+    
+print_board(expertBoard)
+t = process_time()
+solve(expertBoard, [0,0])
 print("_______________________")
-print_board(board)
+print_board(expertBoard)
+elapsed_time = process_time() - t
+elapsed_ms = elapsed_time * 1000.0
+print("Solving this puzzle took " + str(steps) + " steps and " + str(elapsed_ms) + "ms.")
+
